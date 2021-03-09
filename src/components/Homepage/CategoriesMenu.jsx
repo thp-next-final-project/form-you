10bd981297967cdfe5e39d146f60f166d5129802
berangereh <hermann.berangere@gmail.com>
2021-03-09 18:06:28 +0100

    homepage

---
+++ src/components/Homepage/CategoriesMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../styles/CategoriesMenu.css';
+
+
+export default function CategoriesMenu({title}) {
+
+  return (
+    <div>
+        {title}
+    </div>
+  );
+}
---
+++ src/components/Homepage/Formations.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import FormationCard from './FormationCard';
+
+const Formations = () => {
+    const [formations, setFormations] = useState([]);
+
+
+    const fetchFormations = (searchValue) => {
+                
+        fetch(`https://form-you-api.herokuapp.com/formations`)
+        .then(response => response.json())
+        .then(data => {
+            setFormations(data);    
+        });
+      };
+
+      useEffect(()=>{
+          fetchFormations()        
+      },[]);
+
+      
+      return (     
+    
+    <div>
+      
+        {formations.map(formation => (        
+          <FormationCard
+            title={formation.title}
+            id={formation.id}
+            description={formation.description}            
+          />        
+      ))}
+    </div>
+     
+      
+      );
+      
+
+};
+
+export default Formations;
